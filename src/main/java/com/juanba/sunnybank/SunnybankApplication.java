0436eafdb968937860864f1da38116728ab32a6a
JuanBarbosaMora <portelnom0308@gmail.com>
2025-06-19 19:32:20 -0500

    first commit sunnybank

---
+++ src/main/java/com/juanba/sunnybank/SunnybankApplication.java
@@ -0,0 +1,13 @@
+package com.juanba.sunnybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanba/sunnybank/SunnybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanba.sunnybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
