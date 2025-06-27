9b25c11acf4090f803ac9f77669c6226935aa765
JuanBarbosaMora <portelnom0308@gmail.com>
2025-06-27 16:00:05 -0500

    add invalid field on request

--- src/main/java/com/juanba/sunnybank/infrastructure/exceptions/InvalidFieldRequestException.java
+++ src/main/java/com/juanba/sunnybank/infrastructure/exceptions/InvalidFieldRequestException.java
@@ -1,4 +1,27 @@
 package com.juanba.sunnybank.infrastructure.exceptions;
 
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class InvalidFieldRequestException {
+
+    @ExceptionHandler(InvalidDefinitionException.class)
+    public ResponseEntity invalidField(InvalidDefinitionException exception) {
+        var error = exception.getMessage();
+        return ResponseEntity.badRequest().body(new ResponseInvalidField("Invalid field on request", "Duplicate or overwritten field in the request"));
+    }
+
+    public record ResponseInvalidField(
+            String typeError,
+            String message
+    ) {
+        public ResponseInvalidField(String typeError, String message) {
+            this.typeError = typeError;
+            this.message = message;
+        }
+    }
 }
